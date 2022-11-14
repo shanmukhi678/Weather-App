418312efbce182f4e9d555abfeee742742960ff2
shanmukhi678 <73278379+shanmukhi678@users.noreply.github.com>
2022-11-14 20:43:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants={
+    openWeatherMap:{
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"6fa158db8728d155b33b39de7a08305a"
+    }
+}
+
+module.exports=constants;
